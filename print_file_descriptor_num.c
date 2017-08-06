45a2692dcfc2b98ed14737405a18901424f72744
johncanthony <john.claud.anthony@gmail.com>
2017-08-05 20:25:36 -0700

    Added more comments to print_file_descriptor.c

--- print_file_descriptor_num.c
+++ print_file_descriptor_num.c
@@ -14,6 +14,10 @@
    refer to a different file.  For further details on open file
    descriptions, see NOTES.
   '''
+
+  Do Some "Light" Reading:
+  http://man7.org/linux/man-pages/man2/open.2.html
+  http://man7.org/linux/man-pages/man2/write.2.html
 */
 
 
@@ -29,7 +33,8 @@ int main()
 
 
 
-   //Open file and obtain process specific file descriptor  
+   //Open file and obtain process specific file descriptor
+   //*keep in mind we are using the O_RDONLY flag to open the file only for reading* 
    int fd = open("hello_world.txt",O_RDONLY);
 
    //When open fails it will return a `-1` for the file descriptor, we need to catch this error
@@ -40,10 +45,10 @@ int main()
    char fd_hr[11]={0x0};
    sprintf(fd_hr,"%d",fd);
 
+   // We could have simply imported stdio.h and used printf(), but this is about sys calls and file descriptors damn it!
+   // Note we are passing the file descriptor of 1 for STDOUT to print to the terminal
    write(STDOUT, fd_hr, sizeof(fd_hr));
    write(STDOUT, "\n", 3);
-
-
 
 
    return 0;
